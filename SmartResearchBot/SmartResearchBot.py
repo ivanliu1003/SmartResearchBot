eb4c34e75de1b379ddd34e29c5105d34a3286952
ivanliu1003 <liuzhaoheng079@gmail.com>
2026-01-01 12:53:39 +0800

    task1  complete

--- SmartResearchBot/SmartResearchBot.py
+++ SmartResearchBot/SmartResearchBot.py
@@ -1,1 +1,10 @@
-print("helloworld")
+from google import genai
+
+client = genai.Client(api_key=None)
+prompt=input("type what you want to ask:")
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents=prompt,
+)
+
+print(response.text)
